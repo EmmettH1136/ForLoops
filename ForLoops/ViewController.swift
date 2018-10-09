2247d5a4cf76ada354e2ae2b2142ab45e2926434
Emmett Hasley <Ehasley1136@jh-mba158-06.d214.org>
2018-10-09 11:42:05 -0500

    Added basic working for reverse text

--- ForLoops/ViewController.swift
+++ ForLoops/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+var list = ["here"]
+
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var result: UILabel!
+    @IBOutlet weak var textFeeeld: UITextField!
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func whenButtonPressed(_ sender: Any) {
+        list.removeAll()
+        for x in textFeeeld.text! {
+            list.append(String(x))
+        }
+        var list2 = ["J"]
+        var nubmers = list.count
+        list2.removeAll()
+        while nubmers > 0 {
+            list2.append(list[nubmers - 1])
+            nubmers -= 1
+        }
+        nubmers = list2.count
+        if nubmers == 0 {
+            result.text = "Nope"
+        } else {
+            
+            list2[nubmers - 1] = list2[nubmers - 1].lowercased()
+            list2[0] = list2[0].uppercased()
+            let wwww = list2.joined(separator: "")
+            result.text = "\(wwww)"
+        }
+        
+    }
+    
 }
 
