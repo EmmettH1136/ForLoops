2f1198aee0dffbd3f747db2e8ab5712c76e0d8ee
Emmett Hasley <Ehasley1136@jh-mba158-06.d214.org>
2018-10-11 11:42:19 -0500

    Added a lot

--- ForLoops/ViewController.swift
+++ ForLoops/ViewController.swift
@@ -11,23 +11,34 @@ var list = ["here"]
 struct Word {
     var imagee : String
     var correct : String
-    init(_ beep : String, _ weep : String) {
+    var alternate : String
+    var alternate2 : String
+    var alreadyUsed : Bool
+    init(_ beep : String, _ weep : String,_ sheep : String,_ yoop : Bool) {
         self.imagee = beep
-        self.correct = weep
+        self.correct = beep
+        self.alternate = weep
+        self.alternate2 = sheep
+        self.alreadyUsed = yoop
+    }
+    func reset() {
+        for value in 0...(elements.count - 1) {
+            elements[value].alreadyUsed = false
+        }
     }
 }
-let bee = Word("Bee", "Bee")
-let death = Word("Death", "Death")
-let gate = Word("Gate", "Gate")
-let leg = Word("Leg", "Leg")
-let circle = Word("Circle", "Circle")
-let coal = Word("Coal", "Coal")
-let feast = Word("Feast", "Feast")
-let lizards = Word("Lizards", "Lizards")
-let rice = Word("Rice", "Rice")
-let stop = Word("STOP", "Stop")
-let umbrella = Word("Umbrella", "Umbrella")
-let elements = [bee, death, gate, leg, circle, coal, feast, lizards, rice, stop, umbrella]
+let bee = Word("Bee", "Bee", "Bumble Bee", false)
+let death = Word("Death", "Death", "Death", false)
+let gate = Word("Gate", "Gate", "Gate", false)
+let leg = Word("Leg", "Leg", "Pelvis", false)
+let circle = Word("Circle", "Circle", "Circle", false)
+let coal = Word("Coal", "Rocks", "Coal", false)
+let feast = Word("Feast", "Food", "Feast", false)
+let lizards = Word("Lizards", "Geckos", "Lizard", false)
+let rice = Word("Rice", "Rice", "Rice", false)
+let stop = Word("STOP", "Stop", "Stop", false)
+let umbrella = Word("Umbrella", "Umbrella", "Umbrella", false)
+var elements = [bee, death, gate, leg, circle, coal, feast, lizards, rice, stop, umbrella]
 var currentQ = bee
 
 class ViewController: UIViewController {
@@ -63,16 +74,25 @@ class ViewController: UIViewController {
             list2[0] = list2[0].uppercased()
             let wwww = list2.joined(separator: "")
             result.text = "\(wwww)"
-            if result.text == currentQ.correct {
+            if result.text == currentQ.correct || result.text == currentQ.alternate || result.text == currentQ.alternate2 {
                 rightImage.image = UIImage(named: "check")
+                currentQ.alreadyUsed = true
                 currentQ = elements.randomElement()!
-                imageView.image = UIImage(named: currentQ.imagee)
+                var quirch = 10
+                if currentQ.alreadyUsed {
+                    currentQ = elements.randomElement()!
+                } else {
+                    imageView.image = UIImage(named: currentQ.imagee)
+                            
+                }
+                quirch -= 1
             } else {
-                rightImage.image = UIImage(named: "X")
+                    rightImage.image = UIImage(named: "X")
+                }
             }
         }
-        
-    }
-    
 }
+
+
+
 
